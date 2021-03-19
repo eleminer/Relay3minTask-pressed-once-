ee00324906f7ad1d6524573a4e969362ea87a27f
Marc <pansmarcjoe@t-online.de>
2021-03-19 15:18:23 +0100

    added a2

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,4 +1,4 @@
-const int pushbutton = 2; //An PIN 2 wird der Taster angeschlossen
+const int pushbutton = A2; //An PIN 2 wird der Taster angeschlossen
 const int relay = 3; //An PIN 3 wird das Relay angeschlossen
 unsigned long previousMillisInterval = 0;
 unsigned long previousMillisButton = 0;
@@ -12,14 +12,13 @@ void setup() {
   digitalWrite(12, HIGH); pinMode(12, OUTPUT);
   digitalWrite(LED_BUILTIN, LOW); pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(relay, LOW); pinMode(relay, OUTPUT);
-  pinMode(pushbutton, INPUT_PULLUP);
   Serial.begin(9600);
 }
 
 void loop() {    
   currentMillis = millis();
-  if (digitalRead(pushbutton) == HIGH){stateButton = 0;}
-  if (digitalRead(pushbutton) == LOW && stateButton == 0){pushTimeReset(); Serial.println("pressed"); stateButton = 1;}
+  if (analogRead(pushbutton) >= 1000){stateButton = 0;}
+  if ((analogRead(pushbutton) <= 100) && stateButton == 0){pushTimeReset(); Serial.println("pressed"); stateButton = 1;}
   if (previousMillisButton >= currentMillis)
   {
   
